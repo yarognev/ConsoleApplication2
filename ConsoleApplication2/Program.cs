7fa73585e6e560de7dc91a91c6bece69defca538
yarognev <yarognev@gmail.com>
2016-12-30 13:33:54 +0300

    Update Program.cs

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication2 {
     class Program {
-        static void Maingt(string[] args) {
+        static void Main(string[] args) {
         //xyu  mudak
         }
     }
