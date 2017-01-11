4e40f8e646355f5d90464e6d550c0a8768af197b
yarognev <yarognev@gmail.com>
2017-01-11 15:43:19 +0300

    Update Program.cs

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication2 {
     class Program {
-        static void Maing(string[] args) {
+        static void Maingf(string[] args) {
         //xyu  muddakasdf
         }
     }
