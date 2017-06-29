009518dcbd1790caa25ee751620f234f59ca30bb
yarognev <yarognev@gmail.com>
2017-06-29 10:20:34 +0300

    Update Program.cs

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -6,9 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApplication2 {
-    class Program {5sd4
+    class Program {5sd4f
         static void Main(string[] args) {
-        //xyu sd sa
+        //xyu sd saf
         }
     }
 }
