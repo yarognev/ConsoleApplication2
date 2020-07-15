9776fb7c3d524536faf8e38a33ab993fdd87b8a2
yarognev <yarognev@gmail.com>
2020-07-15 15:40:04 +0300

    Update Program.cs

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication2 {
     class Program {
-        static void Main(string[] args) {//obrigan
+        static void Main(string[] args) {//obrigan suka
         }
     }
 }
