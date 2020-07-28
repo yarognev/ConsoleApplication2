481f872cba1bf9e5d69f934e3d35cce5734c71ce
yarognev <yarognev@gmail.com>
2020-07-28 17:13:55 +0300

    Update Tests.cs

--- ConsoleApplication2/Tests.cs
+++ ConsoleApplication2/Tests.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication2 {//eba govno
+namespace ConsoleApplication2 {//eba govno ebannoe
     [TestFixture]
     public class Tests {
         [Test]
